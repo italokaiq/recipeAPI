872aef3eb05a462263e72681054f49846a891a0e
italokaiq <italo.kaiq14@gmail.com>
2024-03-15 16:00:52 -0300

    att recipes com as novas rotas

--- src/routes/recipe.router.ts
+++ src/routes/recipe.router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { RecipeControllers } from "../controllers/recipe.controller";
 import { IsRecipeIdValid } from "../middleware/isRecipeIdValid.middleware";
+import { BodyValidator } from "../middleware/bodyValidator.middleware";
+import { recipeCreateDataSchema, recipeUpdateDataSchema } from "../schemas/recipe.schema";
 
 export const recipeRouter = Router();
 
@@ -8,6 +10,6 @@ const recipeController = new RecipeControllers();
 
 recipeRouter.get("/", recipeController.getMany);
 recipeRouter.get("/:id", IsRecipeIdValid.execute, recipeController.getOne);
-recipeRouter.post("/", recipeController.create);
-recipeRouter.patch("/:id", IsRecipeIdValid.execute, recipeController.update);
+recipeRouter.post("/", BodyValidator.execute(recipeCreateDataSchema), recipeController.create);
+recipeRouter.patch("/:id", BodyValidator.execute(recipeUpdateDataSchema), IsRecipeIdValid.execute, recipeController.update);
 recipeRouter.delete("/:id", IsRecipeIdValid.execute, recipeController.delete);

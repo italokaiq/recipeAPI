44bccd0d1e40a87b0b51e50a5e48790531874f13
italokaiq <italo.kaiq14@gmail.com>
2024-03-15 16:06:07 -0300

    instalando Helmet

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,11 @@ import "express-async-errors";
 import express, { json } from "express";
 import { recipeRouter } from "./routes/recipe.router";
 import { HandleError } from "./errors/handleError";
+import helmet from "helmet";
 
 export const app = express();
 
+app.use(helmet());
 app.use(json());
-
 app.use("/recipes", recipeRouter);
-
 app.use(HandleError.execute);
